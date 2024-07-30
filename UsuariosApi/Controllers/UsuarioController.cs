e3b3b9b943f157c09ac9fb590fdd4d233e3681b1
Eduardo <eduardoj.schroeder@gmail.com>
2024-07-30 16:16:22 -0300

    Commit com as alterações, referente ao login e geração de token.

--- UsuariosApi/Controllers/UsuarioController.cs
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Services;
 
 namespace UsuariosApi.Controllers;
 
@@ -7,9 +11,24 @@ namespace UsuariosApi.Controllers;
 [Route("[Controller]")]
 public class UsuarioController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+    private UsuarioService _usuarioService;
+
+    public UsuarioController(UsuarioService cadastroService)
     {
-        throw new NotImplementedException();
+        _usuarioService = cadastroService;
+    }
+
+    [HttpPost("cadastro")]
+    public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+    {
+        await _usuarioService.Cadastra(dto);
+        return Ok("Usuário cadastrado!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync(LoginUsuarioDto dto)
+    {
+        var token = await _usuarioService.Login(dto);
+        return Ok(token);
     }
 }
---
+++ UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services;
+
+public class TokenService
+{
+    public string GenerateToken(Usuario usuario)
+    {
+        // Dados do token
+        Claim[] claims = new Claim[]
+        {
+            new Claim("username", usuario.UserName),
+            new Claim("id", usuario.Id),
+            new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString())
+        };
+
+        // Gera a chave com base na string
+        var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MUEyQzRFRkYxMjM0NTY3ODlBQkNERUZHSElKS0xNTk8="));
+
+        var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+        // Gera o token
+        var token = new JwtSecurityToken(expires: DateTime.Now.AddSeconds(10), claims: claims, signingCredentials: signingCredentials);
+
+        // Converte para string e retorna
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
